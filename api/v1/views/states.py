774edee0e3a099d6ef6151d8ea1d78b941a2f65a
iakev <kirimikmwongera@gmil.com>
2023-02-17 05:05:24 +0000

    modified for pycodestyle

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,7 +7,6 @@ from models import storage
 from models.state import State
 
 
-
 @app_views.route('/states/', methods=['GET', 'POST'])
 @app_views.route('/states', methods=['GET', 'POST'])
 def states():
